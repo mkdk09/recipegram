8f602da44a56cfe43ddc964cdc1679779611c678
mkdk09 <mkdk099@gmail.com>
2020-11-08 14:58:29 +0900

    ユーザの編集を実装

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'a0ba8d2eabda0a1384ad60444bdea9ac576171cac3da76796c568a9aeef0e1e9104bd2869ecf5a36d950923b34ac9c176daedb9bebcd4593b7184e33194a50c3'
